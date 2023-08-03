583c0aa42ef78b841677c8e4f0ae230e6c3afbaf
Aritra Das <97398407+AritraDasIT@users.noreply.github.com>
2023-08-03 20:08:51 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,13 @@
+a=10;
+b=1.2;
+c=True;
+d="Aritra";
+e= None;
+f=complex(8,2);
+
+print("The type of a is : " , type(a));
+print("The type of b is : " , type(b));
+print("The type of c is : " , type(c));
+print("The type of d is : " , type(d));
+print("The type of e is : " , type(e));
+print("The type of f is : " , type(f));
