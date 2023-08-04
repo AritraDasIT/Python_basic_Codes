3e2563e6028037c68dc361de1888d68c8e8b975f
Aritra Das <97398407+AritraDasIT@users.noreply.github.com>
2023-08-04 08:48:20 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+
+print("Addition of the two values is", a + b)
+print("Subtraction of the two values is", a - b)
+print("Multiplication of the two values is", a * b)
+print("Division of the two values is", a / b)
+print("Modulus of the two values is", a % b)       #Modulus
+print("Exponent of the two values is", a ** b)       #Exponent
+print("Floor division of the two values is", a // b)  #Floor
