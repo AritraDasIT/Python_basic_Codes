c7e91fc2bf47dc98b93d8c2f11d83c537f35fe76
Aritra Das <97398407+AritraDasIT@users.noreply.github.com>
2023-08-04 08:50:02 +0530

    Create typecast.py

---
+++ typecast.py
@@ -0,0 +1,17 @@
+#The conversion of one data type into the other data type is known as type casting in python or type conversion in python. Python supports a wide variety of functions or methods like: int(), float(), str(), ord(), hex(), oct(), tuple(), set(), list(), dict(), etc. for the type casting in pytho
+
+#Explicit typecasting:
+# Explicit type conversion takes place when the programmer clearly and explicitly defines the same in the program.
+
+A= "15";
+B= 8;
+
+print(int(A)+B);  
+
+#Implicit typecasting:
+#In Python, when the data type conversion takes place during compilation or during the runtime, it's called an implicit data type conversion. Python handles the implicit data type conversion.
+
+a=10;
+b=1.5;
+print(a+b);
+
