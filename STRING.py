18dd334e3e4fc503e0f319eecf9e20fda85d0857
Aritra Das <97398407+AritraDasIT@users.noreply.github.com>
2023-08-05 16:38:00 +0530

    Add files via upload

---
+++ STRING.py
@@ -0,0 +1,44 @@
+# STRING 
+
+name="Aritra";
+name1='bibek';
+
+print("hello " +name);
+print('hello ' +name1) # we can declare string using '' also 
+
+
+# print "MY NAME IS ARITRA" 
+
+print('"MY NAME IS ARITRA"');            # we have to use (' "" ')
+
+
+
+# Multiple line string
+
+NAME= ''' hey boys ,
+lets ready 
+for the match''';
+              
+print(NAME);             # we use ( ''' or """ ) tripple single quote or tripple double quote
+
+
+
+# Access character of a string 
+ 
+print(name[0]);
+print(name[1]);
+print(name[2]);
+print(name[3]);
+print(name[4]);
+print(name[5]);
+
+# Access character of a string  using loop
+
+for character in NAME:
+    print(character);
+ 
+
+
+
+
+
