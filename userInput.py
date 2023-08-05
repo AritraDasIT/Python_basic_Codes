2168f22f2b7811ab0f687f28777c0f6c5de7ef9a
Aritra Das <97398407+AritraDasIT@users.noreply.github.com>
2023-08-05 16:37:04 +0530

    Add files via upload

---
+++ userInput.py
@@ -0,0 +1,23 @@
+#user input
+
+#1
+
+a=input();
+print("my name is ", a);
+
+#2
+
+b=input("Enter your Name: ");
+print("my name is ", b);
+
+#3
+
+x=input("Enter the first number: ");
+y=input("Enter the second number: ");
+print(x+y);      #it is a string concatination
+print(int(x)+int(y)); # now typecast into INTEGER 
+
+
+
+
+
