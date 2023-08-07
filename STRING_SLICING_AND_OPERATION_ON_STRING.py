95a0b6b65c7c860992ad59fdbcf3b2cf583cbfd5
Aritra Das <97398407+AritraDasIT@users.noreply.github.com>
2023-08-07 16:53:04 +0530

    Add files via upload

---
+++ STRING_SLICING_AND_OPERATION_ON_STRING.py
@@ -0,0 +1,23 @@
+# STRING SLICING AND OPERATION ON STRING
+
+# LENGTH OF A STRING
+
+Name="My name is Aritra";
+print(Name);
+
+print(len(Name));  #use { len } function 
+
+print(Name[0:8]);  #character access & STRING SLICING ||  #including 0 but not 8
+
+# Negative STRING SLICING
+
+print(Name[0:-8]);   # this means 
+print(Name[0:len(Name)-8]);     
+
+print(Name[-8:-2]);
+
+NAME= "Harry";
+
+print(NAME[-4:-2]);
+
+
